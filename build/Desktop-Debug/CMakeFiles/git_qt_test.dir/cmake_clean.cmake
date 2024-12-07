74a1aa8027ff06f09b601207e0e44af9464e4171
viny_ <viniciuscacarvalho37@gmail.com>
2024-12-07 21:11:39 +0000

    first_try
    fk me up

---
+++ build/Desktop-Debug/CMakeFiles/git_qt_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_qt_test.dir/main.cpp.o"
+  "CMakeFiles/git_qt_test.dir/main.cpp.o.d"
+  "git_qt_test"
+  "git_qt_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/git_qt_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
